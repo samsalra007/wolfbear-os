4720511c646de16aac8cf8fb838bfa07b8f9098b
Samuel Salazar <samsalra.007@gmail.com>
2025-05-28 00:02:51 -0600

    Hello.c basic app

--- overlay/sys-apps/apps/hello/hello.c
+++ overlay/sys-apps/apps/hello/hello.c
@@ -2,13 +2,19 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
+    printf("Iniciando aplicación SDL\n");
+    fflush(stdout);
+
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         fprintf(stderr, "SDL_Init error: %s\n", SDL_GetError());
         return 1;
     }
 
+    printf("SDL inicializado correctamente\n");
+    fflush(stdout);
+
     SDL_Window *window = SDL_CreateWindow(
-        "Hello SDL",
+        "Hola SDL",
         SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
         1366, 768,
         SDL_WINDOW_SHOWN
@@ -19,10 +25,8 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    SDL_Renderer *renderer = SDL_CreateRenderer(
-        window, -1,
-        SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC
-    );
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1,
+        SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
     if (!renderer) {
         fprintf(stderr, "SDL_CreateRenderer error: %s\n", SDL_GetError());
         SDL_DestroyWindow(window);
@@ -30,19 +34,19 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    // Elige un color de fondo (gris medio)
     SDL_SetRenderDrawColor(renderer, 128, 128, 128, 255);
     SDL_RenderClear(renderer);
     SDL_RenderPresent(renderer);
 
-    // Bucle de eventos para mantener la ventana abierta 5 s
-    Uint32 start = SDL_GetTicks();
+    printf("Ventana renderizada\n");
+    fflush(stdout);
+
     SDL_Event evt;
+    Uint32 start = SDL_GetTicks();
     while (SDL_GetTicks() - start < 5000) {
         while (SDL_PollEvent(&evt)) {
             if (evt.type == SDL_QUIT) {
-                start = SDL_GetTicks();  // salimos más rápido
-                break;
+                return 0;
             }
         }
         SDL_Delay(10);
@@ -51,5 +55,7 @@ int main(int argc, char *argv[]) {
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
+    printf("Saliendo de la aplicación\n");
+    fflush(stdout);
     return 0;
 }
