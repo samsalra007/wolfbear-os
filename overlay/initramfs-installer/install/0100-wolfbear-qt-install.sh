34203f9bfa1c83bb211dfd7c02fb26765d5684a9
Samuel Salazar <samsalra.007@gmail.com>
2025-06-12 02:57:47 -0600

    Rolling back compiled qt

--- overlay/initramfs-installer/install/0100-wolfbear-qt-install.sh
+++ overlay/initramfs-installer/install/0100-wolfbear-qt-install.sh
@@ -13,8 +13,8 @@ apk add --root $TARGET_DISK_DIR \
     qt6-qt5compat
 
 
-# debug_message $SCRIPT_NAME "Sincronizando capa de Qt a '$TARGET_DISK_DIR'"
-# rsync -a --links "$INSTALL_RESOURCES_DIR/qt-custom/" "$TARGET_DISK_DIR/"
+debug_message $SCRIPT_NAME "Sincronizando capa de Qt a '$TARGET_DISK_DIR'"
+rsync -a --links "$INSTALL_RESOURCES_DIR/qt-custom/" "$TARGET_DISK_DIR/"
 
 debug_message $SCRIPT_NAME "Ajustando enlaces simbólicos Qt.QtQml"
 mkdir -p "$TARGET_DISK_DIR/usr/include/qt6/QtQml/$QT_VERSION"
